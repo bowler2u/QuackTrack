f849481198a644ddd7987643d162651d0fa1dfe8
CamBow.Dev <34105298+bowler2u@users.noreply.github.com>
2019-05-06 22:14:59 -0700

    Add files via upload
    * Corrected some Syntax and spacing

--- server.js
+++ server.js
@@ -3,109 +3,72 @@
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser')
-const redis = require("redis");
+const redis = require('redis');
 const client = redis.createClient();
-const port = 8080; 
-      
-function start()
-{
-    const app = express();
-    const server = http.createServer(app);
-    let userKey = 0;
-    app.use(express.static('public'));
-    app.use(bodyParser.urlencoded({ extended: true }));
-    
-    //****** Console Tracking for Development ****
-    app.use((req, res, next) =>
-    {
-        console.log('[' + process.pid + '] ' + req.method + ' ' + req.url);
-        next();
-    })
-    
-    client.on('error', (err) => {
-        console.log("Redis Error " + err);
-    })
-    
-    
-    app.get('/', (req, res) => {
-        res.sendFile('main.html', {root:"./public"});
-        
-    })
-    
-    app.post('/submit', (req, res, next) => {
-        let userData = JSON.stringify(req.body);
-        userKey++;
-//        client.INCR('userKeys', err =>{
-//            console.log('user Key Incremented');
-//        })
-        
-        client.set("postId:"+userKey, userData, (error, result) => {
-            if(error){
-                console.log(error);
-                return;
-            }
-            console.log('Set result: ' + result );
-            
-            client.get("postId:"+userKey, (error, result) => {
-                console.log('Get result: ' +  result);
+const port = 8080;
 
-            })
-        })
-        
-        res.sendFile('success.html', {root:"./public"});
-    })
-    
-    // Call this middleware if there is a bad route
-    app.use((req, res, next) => {
-        
-        //res.status(404).sendFile("error.html", {root:"./public"});    SEND AN NICE 404 FORM
-        res.status(404).send("404 Error");
+function start() {
+  const app = express();
+  const server = http.createServer(app);
+  let userKey = 0;
+  app.use(express.static('public'));
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+  //****** Console Tracking for Development ****
+  app.use((req, res, next) => {
+    console.log('[' + process.pid + '] ' + req.method + ' ' + req.url);
+    next();
+
+  });
+
+  client.on('error', (err) => {
+    console.log("Redis Error " + err);
+
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile('main.html', {
+      root: "./public"
     });
 
-    
-    server.listen(port);
-    console.log("Server is running on port: " + port);
-}
+  });
+		
+  app.post('/submit', (req, res, next) => {
+    let userData = JSON.stringify(req.body);
+    userKey++;
 
-module.exports = {"start":start};
+    client.set("postId:" + userKey, userData, (error, result) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      console.log('Set result: ' + result);
 
-/********          REDIS DATA ENTRY EXAMPLE ****************
-    ************************************************************
-    
-    
+      client.get("postId:" + userKey, (error, result) => {
+        console.log('Get result: ' + result);
 
-    client.set('myKey', 'Somevalue', (error, result) => {
-        if(error){
-            console.log(error);
-            return;
-        }
-        
-        console.log('Set result: ' + result );
-        
-        client.get('myKey', (error, result) => {
-            console.log('Get result: ' +  result);
-            
-        })
-    })
-    
-    // Set and retieve a sorted Set in Node
-    
-    client.sadd('mySetKey', 'Hello', 'World', 'Again', (error, result) => {
-        if(error){
-            console.log(error);
-            return;
-        }
-        console.log("Set Add Result : " +  result);
-        
-        client.smembers('mySetKey', (error, result) => {
-            if(error){
-                console.log(error);
-                return;
-            }
-            console.log("Set Get Result : " + result);
-            
-            
-        })
-    })
-    */
+      });
+
+    });
+    res.sendFile('success.html', {
+      root: "./public"
+    });
+
+  });
+
+  // Call this middleware if there is a bad route
+  app.use((req, res, next) => {
+    //res.status(404).sendFile("error.html", {root:"./public"});    SEND AN NICE 404 FORM
+    res.status(404).send("404 Error");
+  });
+
+  server.listen(port);
+  console.log("Server is running on port: " + port);
+};
+
+module.exports = {
+  start: start
+};
 
