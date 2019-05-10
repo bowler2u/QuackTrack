bfd1736f9c794c50279fb27f0ba49557d3ea8f73
CamBow.Dev <34105298+bowler2u@users.noreply.github.com>
2019-05-10 16:00:43 -0700

    Delete redis.test.js
    Folder Name change * Obsolete

--- servJS/redis.test.js
+++
@@ -1,9 +0,0 @@
-const client = require('./redis.js');
-
-test('setData function exists', () => {
-  expect(typeof client.setData).toEqual('function');
-});
-
-test('getData function exists', () => {
-  expect(typeof client.getData).toEqual('function');
-});
